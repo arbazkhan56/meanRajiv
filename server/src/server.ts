4389159d0c25113e051324bf62faf7ccac72adc6
Arbaz <hussainarbaz56@gmail.com>
2024-11-30 19:31:21 +0530

    solve the connecting db issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,30 +16,16 @@ if (!ATLAS_URI) {
   process.exit(1);
 }
 
-// connectToDatabase(ATLAS_URI)
-//   .then(() => {
-//     const app = express();
-//     app.use(cors());
 
-//     app.use("/employees", employeeRouter);
-//     // start the Express server
-//     app.listen(5200, () => {
-//       console.log(`Server running at http://localhost:5200...`);
-//     });
-//   })
-
-const app = express();
-    app.use(cors());
-
-app.use("/employees", employeeRouter);
-    
-//The error indicates that MongoDB is rejecting a document because it failed validation based on the rules defined in the MongoDB schema or the collection's validation criteria.
 connectToDatabase(ATLAS_URI)
   .then(() => {
-    
+    const app = express();
+    app.use(cors());
+    app.use("/employees", employeeRouter);
+
     // start the Express server
-    // app.listen(5200, () => {
+    app.listen(5200, () => {
       console.log(`Server running at http://localhost:5200...`);
-    // });
+    });
   })
   .catch((error) => console.error(error));
